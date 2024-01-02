0da149afcaf29ea05e578f936c117ac8758aa895
Hosein <byt.hossein.bayati@gmail.com>
2024-01-02 11:10:21 +0330

    initial steps for syscall count

--- code/proc.h
+++ code/proc.h
@@ -8,6 +8,8 @@ struct cpu {
   int ncli;                    // Depth of pushcli nesting.
   int intena;                  // Were interrupts enabled before pushcli?
   struct proc *proc;           // The process running on this cpu or null
+
+  int syscalls_count;
 };
 
 extern struct cpu cpus[NCPU];
